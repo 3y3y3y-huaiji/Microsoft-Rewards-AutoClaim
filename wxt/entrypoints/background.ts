5e669721ea47edd5931c05d36b8fb3f699475225
Svit-personal <svit.spindler@gmail.com>
2026-08-02 12:32:47 +0200

    refactor: remove unused Checkbox component and dead check message handler

--- wxt/entrypoints/background.ts
+++ wxt/entrypoints/background.ts
@@ -1,6 +1,6 @@
 import { defineBackground } from '#imports';
 import { browser } from 'wxt/browser';
-import { handleInstallOrUpdate, handleStartup, runRewards, checkLastOpened } from './background/dailySchedule';
+import { handleInstallOrUpdate, handleStartup, runRewards } from './background/dailySchedule';
 import { handleAlarmStep, stopSearches } from './background/searchRunner';
 
 export default defineBackground(() => {
@@ -8,7 +8,6 @@ export default defineBackground(() => {
     browser.runtime.onStartup.addListener(handleStartup);
     browser.runtime.onMessage.addListener((request: { action?: string }) => {
         if (request.action === 'popup') void runRewards(true);
-        else if (request.action === 'check') void checkLastOpened();
         else if (request.action === 'stop') void stopSearches();
     });
     browser.alarms.onAlarm.addListener((alarm) => void handleAlarmStep(alarm));
